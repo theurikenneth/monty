2f357cf55abeed09c31abe12c0d826711a76c2a8
theurikenneth <theurikenneth@gmail.com>
2021-05-14 08:55:36 +0000

    C - Stacks, Queues - LIFO, FIFO

--- bytecodes/000.m
+++ bytecodes/000.m
@@ -1,8 +1,9 @@
-ush 0
-push 1
-push 2
-push 3                                                                                                        pall
-push 4
-push 5
-push 6343
-pall
+push 0$
+push 1$
+push 2$
+  push 3$
+                   pall    $
+push 4$
+    push 5    $
+      push    6        $
+pall$
